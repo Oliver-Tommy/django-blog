b9fc4311739578780a70d671792219df9531cbe6
Thomas Oliver <oliver.tommy@icloud.com>
2024-11-19 12:35:07 +0000

    Add collaboration form

--- about/views.py
+++ about/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
+
 
 # Create your views here
 
@@ -7,10 +10,27 @@ def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        print("received a POST request")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            # collaborate = collaborate_form.save(commit=False)
+            # collaborate.author = request.user
+            # collaborate.post = post
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received.'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
     )
